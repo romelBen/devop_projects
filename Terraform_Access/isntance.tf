82be3b0de4bb7a4fb80cac62e0455392575a8289
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-08-10 00:48:27 -0500

    push for changes with functions

--- Terraform_Access/isntance.tf
+++ Terraform_Access/isntance.tf
@@ -1,5 +1,23 @@
+resource "aws_key_pair" "mykey" {
+  key_name = "mykey"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
+
 resource "aws-isntance" "example" {
   # AWS Linux distro
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.mykey.key_name}"
+
+provisioner "remove-exec" {
+  inline = [
+    "chmod +x /tmp/script.sh",
+    "sudo /tmp/script.sh"
+  ]
+}
+
+connection {
+  user = "${var.INSTANCE_USERNAME}"
+  private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+}
 }
--- Terraform_Access/var.tf
+++ Terraform_Access/var.tf
@@ -1,14 +1,22 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "eu-east-1"
 }
 variable "AMIS" {
     type = "map"
     default = {
+        #These AMIs are Ubuntu
         us-east-1 = "ami-0b25b3d814fa986ce"
         us-east-2 = "ami-0d36f68a8c544bbbe"
         eu-west-1 = "ami-0406237fdb3437aec"
         eu-west-2 = "ami-08cad0202d4a23452"
     }
+}
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "TestKey"  
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "TestKey.pem"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
 }
