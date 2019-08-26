848c7929ba808aa365cba37cb507d6fd42a75b0d
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-08-25 21:18:53 -0500

    pulling a specific subnet

--- Terraform_Access/instance.tf
+++ Terraform_Access/instance.tf
@@ -6,6 +6,14 @@ resource "aws_key_pair" "testkey" {
 resource "aws_instance" "example" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+
+  # VPC Subnet
+  subnet_id = "${aws_subnet.main-public-1.id}"
+
+  # Security Group
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+
+  # Public SSH key
   key_name = "${aws_key_pair.testkey.key_name}"
 
   provisioner "file" {
--- Terraform_Access/securitygroup.tf
+++ Terraform_Access/securitygroup.tf
@@ -3,6 +3,32 @@ data "aws_ip_ranges" "us_ec2" {
     services = [ "ec2" ]
 }
 
+resource "aws_security_group" "allow-ssh" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "allow-ssh"
+  description = "security group that allows ssh and ALL egress traffic"
+
+  egress = {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress = {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh"
+  }
+}
+
+
+/* # Security group created to ALLOW ingress traffic of 443 (HTTPS)
 resource "aws_security_group" "from_us" {
  name = "from_us"
 
@@ -17,3 +43,5 @@ resource "aws_security_group" "from_us" {
     SyncToken = "${data.aws_ip_ranges.us_ec2.sync_token}"
   }
 }
+*/
+
