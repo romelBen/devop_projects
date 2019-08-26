ff0f8100169133c9c2e0bd6a609268a9b3c7a298
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-08-25 21:20:04 -0500

    final changes

--- Terraform_Access/securitygroup.tf
+++ Terraform_Access/securitygroup.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "allow-ssh" {
   name = "allow-ssh"
   description = "security group that allows ssh and ALL egress traffic"
 
-  egress = {
+  egress {
       from_port = 0
       to_port = 0
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress = {
+  ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
