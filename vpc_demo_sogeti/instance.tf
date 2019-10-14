3a4b1ae47c5adb946c6da540cd4d7a25e5b8f4be
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-10-13 22:06:21 -0500

    forgot to add provider

--- vpc_demo_sogeti/instance.tf
+++ vpc_demo_sogeti/instance.tf
@@ -7,7 +7,6 @@ resource "aws_key_pair" "testkey" {
 resource "aws_instance" "webserver" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.testkey.id}"
   subnet_id = "${aws_subnet.public_subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.sg-web.id}"]
   associate_public_ip_address = true
