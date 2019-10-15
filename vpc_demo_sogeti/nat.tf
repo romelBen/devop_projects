0c081ff373ba31ffec73ee2184c6b46612b38c9f
Romel Benavides (CE CONT) <2192362@wholefoods.com>
2019-10-15 08:56:08 -0500

    removed uneccessary info.

--- vpc_demo_sogeti/nat.tf
+++ vpc_demo_sogeti/nat.tf
@@ -12,20 +12,3 @@ resource "aws_nat_gateway" "nat-gw" {
     Name = "NAT Gateway"
   }
 }
-
-
-/*
-# VPC setup for NAT
-resource "aws_route_table" "private-nat" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route { 
-      cidr_block = "0.0.0.0/0"
-      nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
-  }
-
-  tags = {
-      Name = "Main Route Table for NAT subnet"
-  }
-}
-*/
