b9b3ed58e2baf70b340fed13b445905114d4278a
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-10-16 18:55:44 -0500

    changes to route table

--- vpc_demo_sogeti/route-tables.tf
+++ vpc_demo_sogeti/route-tables.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "web-public-rt" {
+resource "aws_route_table" "public_subnet" {
   vpc_id = "${aws_vpc.main.id}"
 
   route {
@@ -11,18 +11,27 @@ resource "aws_route_table" "web-public-rt" {
   }
 }
 
-resource "aws_default_route_table" "default-rt" {
-  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+resource "aws_route_table" "private_subnet" {
+  vpc_id = "${aws_vpc.main.id}"
+
+route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_nat_gateway.nat-gw.id}"
+}
+
+tags = {
+  Name = "Private Subnet RT"
+}
 }
 
 # Assign public subnet to public route table 
-resource "aws_route_table_association" "web-public-rt" {
+resource "aws_route_table_association" "web-public-association" {
   subnet_id = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route_table.web-public-rt.id}"
+  route_table_id = "${aws_route_table.public_subnet.id}"
 }
 
-# Assign privatge subnet to private route table
-resource "aws_route_table_association" "db-private-rt" {
-  subnet_id = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route_table.web-public-rt}"
+# Assign private subnet to private route table
+resource "aws_route_table_association" "db-private-association" {
+  subnet_id = "${aws_subnet.private_subnet.id}"
+  route_table_id = "${aws_route_table.private_subnet.id}"
 }
