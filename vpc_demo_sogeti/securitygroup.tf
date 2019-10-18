95075aa792c218357cdf55ebd7f788b408ece30f
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-10-17 21:31:02 -0500

    updated information

--- vpc_demo_sogeti/securitygroup.tf
+++ vpc_demo_sogeti/securitygroup.tf
@@ -22,14 +22,14 @@ resource "aws_security_group" "sg-web" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["208.84.155.228/32"]
+    cidr_blocks = ["70.121.117.248/32"]
   }
 
   ingress {
     from_port = 3389
     to_port = 3389
     protocol = "tcp"
-    cidr_blocks = ["208.84.155.228/32"]
+    cidr_blocks = ["70.121.117.248/32"]
   }
 
   egress { # SQL Server access to db server
@@ -44,6 +44,20 @@ resource "aws_security_group" "sg-web" {
     to_port = 3306
     protocol = "tcp"
     cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
