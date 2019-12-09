620bf5567bb688e636efa1a4aa7e31efbd0561cb
romelBen <romebenavides101@gmail.com>
2019-12-09 11:16:53 -0600

    ip address changed

--- aws_projects/two_instance_with_asg/securitygroup-v2.tf
+++ aws_projects/two_instance_with_asg/securitygroup-v2.tf
@@ -22,14 +22,14 @@ resource "aws_security_group" "sg-web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["70.121.117.248/32"]
+    cidr_blocks = ["165.231.210.188/32"]
   }
 
   ingress {
     from_port   = 3389
     to_port     = 3389
     protocol    = "tcp"
-    cidr_blocks = ["70.121.117.248/32"]
+    cidr_blocks = ["165.231.210.188/32"]
   }
 
   egress {
