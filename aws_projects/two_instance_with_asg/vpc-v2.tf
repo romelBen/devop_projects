51410ee7a422404ac45f99cf2ef72308ce0acd88
romelBen <romebenavides101@gmail.com>
2019-12-09 15:47:16 -0600

    added dns support

--- aws_projects/two_instance_with_asg/vpc-v2.tf
+++ aws_projects/two_instance_with_asg/vpc-v2.tf
@@ -1,8 +1,8 @@
 # Internet VPC
 resource "aws_vpc" "main" {
   cidr_block           = "${var.vpc_cidr}"
-  enable_dns_support   = "false"
-  enable_dns_hostnames = "false"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
 
   tags = {
     Name = "Terraform VPC"
