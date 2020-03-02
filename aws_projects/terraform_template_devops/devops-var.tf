f78858c5c2fa3a2b75d01452abd17916bb6fee71
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2020-03-01 19:53:25 -0600

    Added RedHat Linux AMI

--- aws_projects/terraform_template_devops/devops-var.tf
+++ aws_projects/terraform_template_devops/devops-var.tf
@@ -22,6 +22,17 @@ variable "public_subnet_cidr" {
   default = "10.0.1.0/24"
 }
 
+variable "azs" {
+  default = "us-east-1a"
+}
+
+variable "AMIS-jenkins" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0c322300a1dd5dc79" # AMI of RedHat Linux
+  }
+}
+
 /*
 variable "private_subnet_cidr" {
   default = "10.0.5.0/24"
@@ -31,20 +42,7 @@ variable "azs" {
   type    = "list"
   default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
 }
-*/
 
-variable "azs" {
-  default = "us-east-1a"
-}
-
-variable "AMIS-jenkins" {
-  type = "map"
-  default = {
-    us-east-1 = "ami-0b69ea66ff7391e80" # AMI is Linux
-  }
-}
-
-/*
 variable "AMIS-db" {
   type = "map"
   default = {
