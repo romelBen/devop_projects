7b95c3f2cf04447fa0325d2263ccdb34ca024de3
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2020-03-02 20:43:14 -0600

    instance config fixed

--- aws_projects/terraform_template_devops/devops-instance.tf
+++ aws_projects/terraform_template_devops/devops-instance.tf
@@ -1,4 +1,5 @@
-resource "aws_instance" "webserver" {
+/*
+ "aws_instance" "webserver" {
   count = 1
   ami = "${lookup(var.AMIS-web, var.AWS_REGION)}"
   instance_type = "t2.micro"
@@ -12,13 +13,14 @@ resource "aws_instance" "webserver" {
     Name = "Webserver"
   }
 }
+*/
 
 resource "aws_instance" "jenkins" {
   count = 1
-  ami = "${lookup(var.AMIS-db, var.AWS_REGION)}"
+  ami = "${lookup(var.AMIS-jenkins, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.sg-db.id}"]
-  subnet_id = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  vpc_security_group_ids = ["${aws_security_group.sg-jenkins.id}"]
+  subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
   key_name = "${var.AWS_KEY_NAME}"
   source_dest_check = false
 
