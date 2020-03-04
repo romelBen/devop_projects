b301da23b09adea8aaab4126b53cc546c8c38ed7
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2020-03-04 00:01:08 -0600

    added shell command to install docker and jenkins

--- aws_projects/terraform_template_devops/jenkins-install.sh
+++ aws_projects/terraform_template_devops/jenkins-install.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
+sudo yum -y update
 
-# Install Java and set PATH
-sudo yum install java-1.8*
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.161-0.b14.e17_4.x86_64/
-# PATH = $PATH:$JAVA_HOME
+# Installation of Java JDK 8
+echo "Install Java JDK 8"
+sudo yum remove -y java
+sudo yum install -y java-1.8.0-openjdk
 
-# Download Jenkins on to the EC2 Instance
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io.redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+# Installation of git
+echo "Install git"
+sudo yum -y git
 
-# Install and start Jenkins
-sudo yum install jenkins
-service jenkins start
+# Installation of Docker engine
+echo "Install Docker engine"
+yum update -yum
+yum install docker -yum
+sudo chkconfig docker on
+
+# Insallation of Jenkins
+echo "Install Jenkins"
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+sudo usermod -a -G docker jenkins
+sudo chkconfig jenkins on
+
+# Start Docker and Jenkins
+sudo service docker start
+sudo service jenkins start
